e37b7fae24caa80a84f8070f21b60fb86d004883
MarvinWaro <mrvnwr.dev@gmail.com>
2024-11-27 14:10:18 +0800

    CRUD ready Employee

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 px-5"> <!-- Added px-5 here -->
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg m-5">
-                <div class="px-5 py-8">
+                <div class="px-5 py-5">
                     Admin Dashboard wew
                 </div>
             </div>
