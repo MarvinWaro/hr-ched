b3741c488ea00bb273b3075f72bc6ba89a4b8545
MarvinWaro <mrvnwr.dev@gmail.com>
2024-12-02 14:16:10 +0800

    Ready for edit
    ready for disabling the admin input fields

--- resources/views/admin/employee/index.blade.php
+++ resources/views/admin/employee/index.blade.php
@@ -20,97 +20,57 @@
                     <table id="export-table">
                         <thead>
                             <tr>
-                                <th>
-                                    <span class="flex items-center">
-                                        ID
-                                        <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
-                                        </svg>
-                                    </span>
-                                </th>
-                                <th>
-                                    <span class="flex items-center">
+                                <th class="bg-gray-800 text-white">
+                                    <span class="flex items-center hover:text-gray-200">
                                         Employee ID
                                         <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
                                         </svg>
                                     </span>
                                 </th>
-                                <th>
-                                    <span class="flex items-center">
+                                <th class="bg-gray-800 text-white">
+                                    <span class="flex items-center hover:text-gray-200">
                                         Name
                                         <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
                                         </svg>
                                     </span>
                                 </th>
-                                <th>
-                                    <span class="flex items-center">
-                                        Email
+                                <th class="bg-gray-800 text-white">
+                                    <span class="flex items-center hover:text-gray-200">
+                                        Department & Position
                                         <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
                                         </svg>
                                     </span>
                                 </th>
-                                <th>
-                                    <span class="flex items-center">
-                                        Role
-                                        <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
-                                        </svg>
-                                    </span>
-                                </th>
-                                <th>
-                                    <span class="flex items-center">
-                                        Gender
-                                        <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
-                                        </svg>
-                                    </span>
-                                </th>
-                                <th>
-                                    <span class="flex items-center">
-                                        Department
-                                        <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
-                                        </svg>
-                                    </span>
-                                </th>
-                                <th>
-                                    <span class="flex items-center">
-                                        Payroll Position
-                                        <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
-                                        </svg>
-                                    </span>
-                                </th>
-                                <th>
-                                    <span class="flex items-center">
+                                <th class="bg-gray-800 text-white">
+                                    <span class="flex items-center hover:text-gray-200">
                                         Designation
                                         <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
                                         </svg>
                                     </span>
                                 </th>
-                                <th>
-                                    <span class="flex items-center">
-                                        Place of Assignment
-                                        <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
-                                        </svg>
-                                    </span>
-                                </th>
-                                <th>
-                                    <span class="flex items-center">
+                                <th class="bg-gray-800 text-white">
+                                    <span class="flex items-center hover:text-gray-200">
                                         Date Employed
                                         <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
                                         </svg>
                                     </span>
                                 </th>
-                                <th>
-                                    <span class="flex items-center">
+                                <th class="bg-gray-800 text-white">
+                                    <span class="flex items-center hover:text-gray-200">
                                         Employment Status
+                                        <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
+                                        </svg>
+                                    </span>
+                                </th>
+                                <th class="bg-gray-800 text-white">
+                                    <span class="flex items-center hover:text-gray-200 ">
+                                        Actions
                                         <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
                                         </svg>
@@ -121,18 +81,49 @@
                         <tbody>
                             @foreach($users as $user)
                             <tr class="hover:bg-gray-50 dark:hover:bg-gray-800 cursor-pointer">
-                                <td class="font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $loop->iteration }}</td>
-                                <td>{{ $user->employee_no }}</td>
-                                <td>{{ $user->name }}</td>
-                                <td>{{ $user->email }}</td>
-                                <td>{{ $user->usertype }}</td>
-                                <td>{{ ucfirst($user->gender) }}</td>
-                                <td>{{ $user->department }}</td>
-                                <td>{{ $user->payroll_position }}</td>
+                                <td class="font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $user->id }} - {{ $user->employee_no }}</td>
+                                <td>{{ $user->name }}<br>{{ $user->email }}</td>
+                                <td>{{ $user->department }}<br>{{ $user->payroll_position }}</td>
                                 <td>{{ $user->designation }}</td>
-                                <td>{{ $user->place_of_assignment }}</td>
                                 <td>{{ $user->date_employed }}</td>
-                                <td>{{ $user->employment_status }}</td>
+                                <td>
+                                    <span class="{{ $user->employment_status === 'Active' ? 'bg-green-500 text-white' : 'bg-red-500 text-white' }} py-1 px-3 rounded-full">
+                                        {{ $user->employment_status }}
+                                    </span>
+                                </td>
+                                <td>
+                                    <button id="dropdownHoverButton-{{ $user->id }}" data-dropdown-toggle="dropdownHover-{{ $user->id }}" data-dropdown-trigger="hover" class="text-gray-800 bg-transparent border border-gray-300 hover:text-gray-500 focus:ring-4 focus:outline-none focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-transparent dark:border-gray-600 dark:text-gray-300 dark:hover:text-gray-400 dark:focus:ring-gray-800" type="button">
+                                        Action
+                                        <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                                        </svg>
+                                    </button>
+                                    <div id="dropdownHover-{{ $user->id }}" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                                        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownHoverButton-{{ $user->id }}">
+                                            <li>
+                                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                                    <i class="fa-solid fa-eye me-2"></i>View
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="#!" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                                    <i class="fa-solid fa-pen-to-square me-2"></i>Edit
+                                                </a>
+                                            </li>
+                                            <hr class="w-[90%] mx-auto">
+                                            <li>
+                                                <form action="#!" method="POST" class="delete-form">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="delete-button w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white flex items-center focus:outline-none">
+                                                        <i class="fa-solid fa-trash me-2 text-red-500"></i>
+                                                        <span class="text-red-500">Delete</span>
+                                                    </button>
+                                                </form>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </td>
                             </tr>
                             @endforeach
                         </tbody>
